a821bde169ce41751464662eb6b0fb339ee894d7
TechnicalShree <krushnasr96@gmail.com>
2025-05-06 01:19:15 +0530

    chore: update Tailwind CSS version and configuration
    - Downgraded Tailwind CSS from version 4 to 3.4.17 in package.json.
    - Updated postcss.config.mjs to use the new Tailwind CSS plugin format.
    - Refactored globals.css to use Tailwind's @tailwind directives instead of @import.
    - Adjusted import path for LoginPage component in page.tsx.
    - Added a new Tailwind configuration file (tailwind.config.ts) with custom theme settings.
    - Changed TypeScript target from ES2017 to ES6 and updated path mappings in tsconfig.json.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,4 @@
-import LoginPage from "@/components/templates/LoginPage";
+import LoginPage from "../../../components/templates/LoginPage";
 import React from "react";
 
 export default function Login() {
