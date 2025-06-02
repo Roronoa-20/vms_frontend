70483cae6c6ec07fd16a87d4830478ae9a1fecba
Itachi-20 <neel.udvadia@merillife.com>
2025-06-02 15:52:43 +0530

    added favicon icon

--- src/components/molecules/navbar.tsx
+++ src/components/molecules/navbar.tsx
@@ -14,8 +14,8 @@ const Navbar = () => {
       <div className='flex items-center gap-4'>
         {/* Notification Icon & Modal */}
         <div className='flex flex-col gap-1 justify-center items-center'>
-          <h1 className='text-xl font-semibold leading-[10px] text-right'>{name}</h1>
-          <h1 className='text-[#5f5f5f] text-right'>{designation}</h1>
+          <h1 className='text-md font-semibold leading-[10px] text-right'>{name}</h1>
+          <h1 className='text-[#5f5f5f] text-md text-right'>{designation}</h1>
         </div>
         {/* <Image className='rounded-full w-12 h-12' src={"/boy.jpg"} alt={""} width={30} height={30} /> */}
         <div onClick={()=>{setIsDialog((prev)=> !prev)}} className="relative cursor-pointer w-8 h-8 rounded-full bg-purple-400 flex items-center justify-center text-white text-xl">
