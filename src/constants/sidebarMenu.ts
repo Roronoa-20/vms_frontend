a20ff31ec743a183018d70cf731b207ff6388c1d
Itachi-20 <neel.udvadia@merillife.com>
2025-06-04 14:04:20 +0530

    hide unused sidebar tabs

--- src/constants/sidebarMenu.ts
+++ src/constants/sidebarMenu.ts
@@ -9,44 +9,44 @@ export const sidebarMenu = [
     name: "Vendor",
     href:"/vendor-registration"
   },
-  {
-    logo: "/sidebar-assests/raise-rfq-logo.svg",
-    name: "Raise RFQ",
-    href:""
-  },
-  {
-    logo: "/sidebar-assests/view-pr.svg",
-    name: "View PR",
-    href:""
-  },
-  {
-    logo: "/sidebar-assests/po-details.svg",
-    name: "View PO",
-    href:""
-  },
-  {
-    logo: "/sidebar-assests/view-grn.svg",
-    name: "View GRN",
-    href:""
-  },
-  {
-    logo: "/sidebar-assests/dispatch-logo.svg",
-    name: "View Dispatch",
-    href:""
-  },
-  {
-    logo: "/sidebar-assests/raise-payment-1.svg",
-    name: "View Payment",
-    href:""
-  },
-  {
-    logo: "/sidebar-assests/purchase-enquiry-logo.svg",
-    name: "Purchase Enquiry",
-    href:""
-  },
-  {
-    logo: "/sidebar-assests/complaint-box.svg",
-    name: "Complain Box",
-    href:""
-  },
+  // {
+  //   logo: "/sidebar-assests/raise-rfq-logo.svg",
+  //   name: "Raise RFQ",
+  //   href:""
+  // },
+  // {
+  //   logo: "/sidebar-assests/view-pr.svg",
+  //   name: "View PR",
+  //   href:""
+  // },
+  // {
+  //   logo: "/sidebar-assests/po-details.svg",
+  //   name: "View PO",
+  //   href:""
+  // },
+  // {
+  //   logo: "/sidebar-assests/view-grn.svg",
+  //   name: "View GRN",
+  //   href:""
+  // },
+  // {
+  //   logo: "/sidebar-assests/dispatch-logo.svg",
+  //   name: "View Dispatch",
+  //   href:""
+  // },
+  // {
+  //   logo: "/sidebar-assests/raise-payment-1.svg",
+  //   name: "View Payment",
+  //   href:""
+  // },
+  // {
+  //   logo: "/sidebar-assests/purchase-enquiry-logo.svg",
+  //   name: "Purchase Enquiry",
+  //   href:""
+  // },
+  // {
+  //   logo: "/sidebar-assests/complaint-box.svg",
+  //   name: "Complain Box",
+  //   href:""
+  // },
 ];
