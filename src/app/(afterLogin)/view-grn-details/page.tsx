7fa43b2474a4b5b0741a1d246e1992f2f3a287ac
Rishi Hingad <rishi.hingad@merillife.com>
2025-08-19 11:23:04 +0530

    GRN Details updated and ASA changes

--- src/app/(afterLogin)/view-grn-details/page.tsx
+++ src/app/(afterLogin)/view-grn-details/page.tsx
@@ -1,43 +1,8 @@
-'use client';
+import React from 'react';
+import ViewGRNDetails from '@/src/components/pages/viewGRNDetails';
 
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import ViewGRNDetails from '@/src/components/templates/ViewGRNDetails';
-import requestWrapper from '@/src/services/apiCall';
-import API_END_POINTS from '@/src/services/apiEndPoints';
-import { GRNForm } from '@/src/types/grntypes';
-
-const ViewGRNDetailPage = () => {
-  const params = useSearchParams();
-  const grn_ref = params.get('grn_ref') || '';
-  const [data, setData] = useState<GRNForm | null>(null);
-
-  useEffect(() => {
-    if (!grn_ref) return;
-
-    const fetchData = async () => {
-      try {
-        const response = await requestWrapper({
-          url: `${API_END_POINTS.SingleGRNdetails}?grn_number=${grn_ref}`,
-          method: 'GET',
-        });
-
-        const result: GRNForm = response?.data?.message;
-        console.log('GRN Data:', result);
-        setData(result);
-      } catch (err) {
-        console.error('Failed to fetch GRN details:', err);
-      }
-    };
-
-    fetchData();
-  }, [grn_ref]);
-
-  return data ? (
-    <ViewGRNDetails grn={data} />
-  ) : (
-    <div className="p-4 text-center">Loading GRN Details...</div>
-  );
+const Page = (): React.ReactElement => {
+  return <ViewGRNDetails />;
 };
 
-export default ViewGRNDetailPage;
+export default Page;
---
+++ src/components/molecules/Dashboard-ASA-Pending-Vendor-List.tsx
@@ -0,0 +1,185 @@
+"use client"
+import React, { useState, useEffect } from "react";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/src/components/atoms/table";
+import {Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue} from "@/src/components/atoms/select";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Input } from "../atoms/input";
+import { ASAFormResponse, TvendorRegistrationDropdown, ASAForm } from "@/src/types/types";
+import requestWrapper from "@/src/services/apiCall";
+import { AxiosResponse } from "axios";
+import API_END_POINTS from "@/src/services/apiEndPoints";
+import Pagination from "./Pagination";
+
+type Props = {
+  dashboardTableData: ASAFormResponse;
+  companyDropdown: TvendorRegistrationDropdown["message"]["data"]["company_master"]
+}
+const useDebounce = (value: any, delay: any) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+const DashboardASAVendorFormTable = ({ dashboardTableData, companyDropdown }: Props) => {
+  console.log("Dashboard Pending table ASA---->",dashboardTableData)
+
+  const [table, setTable] = useState<ASAForm[]>(dashboardTableData?.pending_asa_vendors || []);
+  const [selectedCompany, setSelectedCompany] = useState<string>("")
+  const [search, setSearch] = useState<string>("");
+
+  const [totalCount, setTotalCount] = useState<number>(dashboardTableData?.total_count || 0);
+  const [recordPerPage, setRecordPerPage] = useState<number>(dashboardTableData?.page_length || 5);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const debouncedSearchName = useDebounce(search, 300);
+
+  useEffect(() => {
+    fetchTable();
+  }, [debouncedSearchName, selectedCompany, currentPage])
+
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  // const fetchTable = async () => {
+  //   const url = `${API_END_POINTS.asavendorListdashboard}`;
+  //   const res: AxiosResponse = await requestWrapper({ url, method: "GET" });
+
+  //   if (res?.status === 200 && res.data?.message) {
+  //     const msg = res.data.message;
+  //     console.log("ASA Table Data--->", msg)
+  //     setTable(msg.data || []);
+  //     setTotalCount(msg.total_count || 0);
+  //     setRecordPerPage(msg.page_length || 5);
+  //   }
+  // };
+
+
+  const fetchTable = async () => {
+    const params = new URLSearchParams({
+      page_no: String(currentPage),
+      page_length: String(recordPerPage)
+    });
+    if (debouncedSearchName.trim()) {
+      params.append("vendor_name", debouncedSearchName.trim());
+    }
+    const url = `${API_END_POINTS.asavendorListdashboard}?${params.toString()}`;
+    const res: AxiosResponse = await requestWrapper({ url, method: "GET" });
+
+    if (res?.status === 200 && res.data?.status === "success") {
+      console.log("ASA Table Data--->", res.data);
+      setTable(res.data.data || []);
+      setTotalCount(res.data.total_count || 0);
+      setRecordPerPage(res.data.page_length || 5);
+    }
+  };
+
+
+  const formatDate = (dateStr: string | undefined) => {
+    if (!dateStr) return '-';
+    const dateObj = new Date(dateStr);
+    if (isNaN(dateObj.getTime())) return '-';
+
+    const day = String(dateObj.getDate()).padStart(2, '0');
+    const month = String(dateObj.getMonth() + 1).padStart(2, '0');
+    const year = dateObj.getFullYear();
+
+    return `${day}-${month}-${year}`;
+  };
+
+
+  return (
+    <>
+      <div className="shadow- bg-[#f6f6f7] p-4 rounded-2xl">
+        <div className="flex w-full justify-between pb-4">
+          <h1 className="text-[20px] text-[#03111F] font-semibold">
+            Pending Vendor List
+          </h1>
+          <div className="flex gap-4">
+            <Input
+              placeholder="Search..."
+              value={search}
+              onChange={handleSearchChange}
+            />
+            {/* <Select onValueChange={(value) => { setSelectedCompany(value) }}>
+              <SelectTrigger className="w-96">
+                <SelectValue placeholder="Select Company" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup className="w-full">
+                  {
+                    companyDropdown?.map((item, index) => (
+                      <SelectItem key={index} value={item?.name}>{item?.description}</SelectItem>
+                    ))
+                  }
+                </SelectGroup>
+              </SelectContent>
+            </Select> */}
+          </div>
+        </div>
+        <Table>
+          <TableHeader className="text-center">
+            <TableRow className="bg-[#DDE8FE] text-[#2568EF] text-[14px] hover:bg-[#DDE8FE] text-center">
+              <TableHead className="w-[80px]">Sr No.</TableHead>
+              <TableHead className="text-center">Ref No.</TableHead>
+              <TableHead className="text-center">Vendor Name</TableHead>
+              <TableHead className="text-center">Vendor Ref No</TableHead>
+              <TableHead className="text-center">Created On</TableHead>
+              <TableHead className="text-center">View Detials</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody className="text-center">
+            {table.length > 0 ? (
+              table.map((item, index) => (
+                <TableRow key={item.name}>
+                  <TableCell>{(currentPage - 1) * recordPerPage + index + 1}</TableCell>
+                  <TableCell>{item.name}</TableCell>
+                  <TableCell>{item.vendor_name}</TableCell>
+                  <TableCell>{item.vendor_ref_no}</TableCell>
+                  <TableCell className="text-center">{formatDate(item?.creation)}</TableCell>
+                  <TableCell className="text-center">
+                    <Link href={`/view-asa-form?tabtype=company_information&vms_ref_no=${item?.vendor_ref_no}`}
+                      onClick={() => {
+                        if (item?.vendor_name) {
+                          localStorage.setItem("vendor_name", item.vendor_name);
+                        }
+                      }}>
+                      <Button className="bg-blue-400 text-white hover:bg-white hover:text-black">
+                        View
+                      </Button>
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={5} className="text-center text-gray-500 py-4">
+                  No results found
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <Pagination
+        currentPage={currentPage}
+        record_per_page={recordPerPage}
+        setCurrentPage={setCurrentPage}
+        total_event_list={totalCount}
+      />
+    </>
+  );
+};
+
+export default DashboardASAVendorFormTable;
