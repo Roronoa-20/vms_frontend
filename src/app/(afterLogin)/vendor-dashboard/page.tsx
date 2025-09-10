3e6ebe5d21d7219dfeb431f4807467f0af622dc1
Rishi Hingad <rishi.hingad@merillife.com>
2025-09-10 16:14:15 +0530

    Vendor Dashboard type error

--- src/app/(afterLogin)/vendor-dashboard/page.tsx
+++ src/app/(afterLogin)/vendor-dashboard/page.tsx
@@ -1,9 +1,12 @@
+export const dynamic = "force-dynamic";
+
 import React from 'react'
-import Vendordashboard from '@/src/components/pages/VendorDashboard'
+import Vendordashboard from '@/src/components/pages/VendorDashboard';
+
 const VendorDashboard = () => {
   return (
     <Vendordashboard/>
   )
 }
 
-export default VendorDashboard
+export default VendorDashboard;
