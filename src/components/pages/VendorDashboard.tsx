a471ae689e35eb302bcb10331a403911f6fc0111
Rishi Hingad <rishi.hingad@merillife.com>
2025-09-10 16:10:46 +0530

    Vendor Dashboard type error

--- src/components/pages/VendorDashboard.tsx
+++ src/components/pages/VendorDashboard.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import VendorDashboardClient from "./VendorDashboardClient";
-import { AxiosResponse } from "axios";
 import requestWrapper from "@/src/services/apiCall";
 import API_END_POINTS from "@/src/services/apiEndPoints";
 import {
   dashboardCardData,
   TvendorRegistrationDropdown,
   VendorDashboardPOTableData,
-  RFQTable
+  RFQTable,
 } from "@/src/types/types";
 import { cookies } from "next/headers";
 
@@ -16,40 +15,55 @@ const VendorDashboard = async () => {
     const cookieStore = await cookies();
     const cookieHeaderString = cookieStore
       .getAll()
-      .map(({ name, value }) => `${name}=${value}`)
+      .map((c) => `${c.name}=${c.value}`)
       .join("; ");
 
-    const dashboardCardApi: AxiosResponse = await requestWrapper({
-      url: API_END_POINTS?.VendorCodeBasedCardCount,
-      method: "GET",
-      headers: { cookie: cookieHeaderString },
-    });
+    const [
+      dashboardCardApi,
+      dashboardPOTableApi,
+      dropDownApi,
+      dispatchTableApi,
+      rfqApi,
+    ] = await Promise.all([
+      requestWrapper({
+        url: API_END_POINTS?.VendorCodeBasedCardCount,
+        method: "GET",
+        headers: { cookie: cookieHeaderString },
+      }),
+      requestWrapper({
+        url: API_END_POINTS?.vendorPOTable,
+        method: "GET",
+        headers: { cookie: cookieHeaderString },
+      }),
+      requestWrapper({
+        url: API_END_POINTS?.vendorRegistrationDropdown,
+        method: "GET",
+      }),
+      requestWrapper({
+        url: API_END_POINTS?.dispatchTable,
+        method: "GET",
+        headers: { cookie: cookieHeaderString },
+      }),
+      requestWrapper({
+        url: API_END_POINTS?.rfqTableData,
+        method: "GET",
+        headers: { cookie: cookieHeaderString },
+      }),
+    ]);
+
+    // --- Transform responses ---
     const cardData: dashboardCardData =
       dashboardCardApi?.status === 200 ? dashboardCardApi?.data?.message : [];
 
-    const dashboardPOTableApi: AxiosResponse = await requestWrapper({
-      url: API_END_POINTS?.vendorPOTable,
-      method: "GET",
-      headers: { cookie: cookieHeaderString },
-    });
     const dashboardPOTableData: VendorDashboardPOTableData["message"] =
       dashboardPOTableApi?.status === 200
         ? dashboardPOTableApi?.data?.message
         : [];
 
-    const dropDownApi: AxiosResponse = await requestWrapper({
-      url: API_END_POINTS?.vendorRegistrationDropdown,
-      method: "GET",
-    });
     const dropdownData: TvendorRegistrationDropdown["message"]["data"] =
       dropDownApi?.status === 200 ? dropDownApi?.data?.message?.data : {};
     const companyDropdown = dropdownData?.company_master || [];
 
-    const dispatchTableApi: AxiosResponse = await requestWrapper({
-      url: API_END_POINTS?.dispatchTable,
-      method: "GET",
-      headers: { cookie: cookieHeaderString },
-    });
     const dispatchTableData =
       dispatchTableApi?.status === 200
         ? dispatchTableApi?.data?.message?.dispatches
@@ -59,16 +73,10 @@ const VendorDashboard = async () => {
         ? dispatchTableApi?.data?.message?.card_count
         : 0;
 
-    const rfqDashboardUrl = API_END_POINTS?.rfqTableData;
-    const rfqApi: AxiosResponse = await requestWrapper({
-      url: rfqDashboardUrl,
-      method: "GET",
-      headers: { cookie: cookieHeaderString },
-    });
     const rfqData: RFQTable =
-      rfqApi?.status == 200 ? rfqApi?.data?.message : "";
-    console.log(rfqData, "this is rfqData");
+      rfqApi?.status === 200 ? rfqApi?.data?.message : "";
 
+    // --- Render Client Component ---
     return (
       <VendorDashboardClient
         companyDropdown={companyDropdown}
@@ -88,6 +96,98 @@ const VendorDashboard = async () => {
 };
 
 export default VendorDashboard;
+
+
+// import React from "react";
+// import VendorDashboardClient from "./VendorDashboardClient";
+// import { AxiosResponse } from "axios";
+// import requestWrapper from "@/src/services/apiCall";
+// import API_END_POINTS from "@/src/services/apiEndPoints";
+// import {
+//   dashboardCardData,
+//   TvendorRegistrationDropdown,
+//   VendorDashboardPOTableData,
+//   RFQTable
+// } from "@/src/types/types";
+// import { cookies } from "next/headers";
+
+// const VendorDashboard = async () => {
+//   try {
+//     const cookieStore = await cookies();
+//     const cookieHeaderString = cookieStore
+//       .getAll()
+//       .map(({ name, value }) => `${name}=${value}`)
+//       .join("; ");
+
+//     const dashboardCardApi: AxiosResponse = await requestWrapper({
+//       url: API_END_POINTS?.VendorCodeBasedCardCount,
+//       method: "GET",
+//       headers: { cookie: cookieHeaderString },
+//     });
+//     const cardData: dashboardCardData =
+//       dashboardCardApi?.status === 200 ? dashboardCardApi?.data?.message : [];
+
+//     const dashboardPOTableApi: AxiosResponse = await requestWrapper({
+//       url: API_END_POINTS?.vendorPOTable,
+//       method: "GET",
+//       headers: { cookie: cookieHeaderString },
+//     });
+//     const dashboardPOTableData: VendorDashboardPOTableData["message"] =
+//       dashboardPOTableApi?.status === 200
+//         ? dashboardPOTableApi?.data?.message
+//         : [];
+
+//     const dropDownApi: AxiosResponse = await requestWrapper({
+//       url: API_END_POINTS?.vendorRegistrationDropdown,
+//       method: "GET",
+//     });
+//     const dropdownData: TvendorRegistrationDropdown["message"]["data"] =
+//       dropDownApi?.status === 200 ? dropDownApi?.data?.message?.data : {};
+//     const companyDropdown = dropdownData?.company_master || [];
+
+//     const dispatchTableApi: AxiosResponse = await requestWrapper({
+//       url: API_END_POINTS?.dispatchTable,
+//       method: "GET",
+//       headers: { cookie: cookieHeaderString },
+//     });
+//     const dispatchTableData =
+//       dispatchTableApi?.status === 200
+//         ? dispatchTableApi?.data?.message?.dispatches
+//         : [];
+//     const dispatchCardCount =
+//       dispatchTableApi?.status === 200
+//         ? dispatchTableApi?.data?.message?.card_count
+//         : 0;
+
+//     const rfqDashboardUrl = API_END_POINTS?.rfqTableData;
+//     const rfqApi: AxiosResponse = await requestWrapper({
+//       url: rfqDashboardUrl,
+//       method: "GET",
+//       headers: { cookie: cookieHeaderString },
+//     });
+//     const rfqData: RFQTable =
+//       rfqApi?.status == 200 ? rfqApi?.data?.message : "";
+//     console.log(rfqData, "this is rfqData");
+
+//     return (
+//       <VendorDashboardClient
+//         companyDropdown={companyDropdown}
+//         cardData={cardData}
+//         dashboardPOTableData={dashboardPOTableData}
+//         dispatchTableData={dispatchTableData}
+//         dispatchCardCount={dispatchCardCount}
+//         rfqData={rfqData}
+//       />
+//     );
+//   } catch (error) {
+//     console.error("VendorDashboard fetch error:", error);
+//     return (
+//       <div className="p-4 text-red-600">Error loading Vendor Dashboard</div>
+//     );
+//   }
+// };
+
+// export default VendorDashboard;
 
 // "use client";
 
