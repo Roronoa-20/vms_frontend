14aa34b9bb219704d392ebf00cf4a1408ac86fbc
goursunder <atul.chune@merillife.com>
2025-09-16 12:03:50 +0530

    build error solved

--- src/components/templates/RFQTemplates/LogisticsReviseRFQ.tsx
+++ src/components/templates/RFQTemplates/LogisticsReviseRFQ.tsx
@@ -98,7 +98,7 @@ const LogisticsReviseRFQ = ({ open, onClose, Dropdown, RFQData }: Props) => {
                     <DialogTitle>
                         <div className='flex justify-between items-center pr-4'>
                             <h1 className='font-bold text-[24px] p-4 '>Revise RFQ</h1>
-                            <Button onClick={handleOpen}>Add New Vendor</Button>
+                            {/* <Button onClick={handleOpen}>Add New Vendor</Button> */}
                         </div>
                     </DialogTitle>
                 </DialogHeader>
@@ -123,7 +123,7 @@ const LogisticsReviseRFQ = ({ open, onClose, Dropdown, RFQData }: Props) => {
                     <Pagination currentPage={currentVendorPage} setCurrentPage={setVendorCurrentPage} record_per_page={VendorList?.data.length ? VendorList?.data.length : 0} total_event_list={VendorList?.total_count ? VendorList?.total_count : 0} />
                 </div>}
                 <div className='py-6'>
-                    <NewVendorTable newVendorTable={newVendorTable} />
+                    <NewVendorTable newVendorTable={newVendorTable} handleOpen={handleOpen}/>
                 </div>
 
                 {
