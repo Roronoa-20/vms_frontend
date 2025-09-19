a9c52bf7fc409c7d7d73ce80b43a817c2365bedf
Tech.Support <neel.udvadia@merillife.com>
2025-09-19 10:18:50 +0530

    added maintainance page

---
+++ src/app/maintainance/page.tsx
@@ -0,0 +1,10 @@
+import MaintenancePage from '@/src/components/molecules/maintenance-page'
+import React from 'react'
+
+const page = () => {
+  return (
+    <MaintenancePage/>
+  )
+}
+
+export default page;
