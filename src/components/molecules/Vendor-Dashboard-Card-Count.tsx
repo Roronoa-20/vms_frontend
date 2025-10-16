fd8f86907f06d3e2d9d121ff3e399259b3339939
Tech.Support <neel.udvadia@merillife.com>
2025-10-16 15:25:23 +0530

    added api call on vendor details page on vendor code selecton

--- src/components/molecules/Vendor-Dashboard-Card-Count.tsx
+++ src/components/molecules/Vendor-Dashboard-Card-Count.tsx
@@ -29,6 +29,7 @@ const VendorDashboardCards = ({ ...Props }: Props) => {
   const [open, setOpen] = useState(false);
   const [cardCount, setCardCount] = useState(Props?.cardData);
   const dropdownRef = useRef<HTMLDivElement>(null);
+  const [poTAble,setPoTable] = useState(Props?.dashboardPOTableData);
 
   useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
@@ -55,7 +56,26 @@ const VendorDashboardCards = ({ ...Props }: Props) => {
       }
     }
     fetchCardCount();
+
+    if(selectedVendorCode){
+      
+    }  
+
+
   }, [selectedVendorCode])
+
+
+  const getPoDetail = async()=>{
+    const response = await requestWrapper({
+        url: API_END_POINTS?.vendorPOTable,
+        method: "GET",
+      })
+
+      if(response?.status == 200){
+        setPoTable(response?.data?.message)
+      }
+  }
+
 
   const cardData = [
     {
@@ -208,7 +228,7 @@ const VendorDashboardCards = ({ ...Props }: Props) => {
           return (
 
             <TabsContent key={item.name || index} value={item.name}>
-              {item.name === "Purchase & Ongoing Orders" && <PurchaseAndOngoingOrders dashboardPOTableData={Props?.dashboardPOTableData} companyDropdown={Props?.companyDropdown} />}
+              {item.name === "Purchase & Ongoing Orders" && <PurchaseAndOngoingOrders dashboardPOTableData={poTAble} companyDropdown={Props?.companyDropdown} />}
               {item.name === "Quotation" && <DashboardRFQTable dashboardTableData={Props?.rfqData?.data} companyDropdown={Props?.companyDropdown} />}
               {/* {item.name === "Dispatch Details" && <DashboardApprovedVendorsTable dashboardTableData={Props.dashboardApprovedVendorTableData} companyDropdown={Props?.companyDropdown}/>} */}
               {item.name === "Dispatch Details" && <DashboardDispatchVendorsTable dashboardTableData={Props?.dispatchTableData} />}
