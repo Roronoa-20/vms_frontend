b72d4c42480a0237d6231f9e7d4ed06a62c61c1f
Rishi Hingad <rishi.hingad@merillife.com>
2025-11-02 18:15:02 +0530

    Material ONboarding CHanges

---
+++ src/app/(afterLogin)/view-material-code-request/page.tsx
@@ -0,0 +1,10 @@
+import MaterialOnboarding from '@/src/components/pages/view-material-code-request'
+import React from 'react'
+
+const page = () => {
+  return (
+    <MaterialOnboarding/>
+  )
+}
+
+export default page
