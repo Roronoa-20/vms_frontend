7ae312f19756fba49f806e4cd2be4c81c0e25e9e
neeludvadia <neel.udvadia@merillife.com>
2026-03-06 15:42:21 +0530

    showed server message on submit pr button incase of error comes from sap

--- src/components/pages/PRRequest.tsx
+++ src/components/pages/PRRequest.tsx
@@ -9,80 +9,80 @@ import { getPurchaseReqisitionData, submitPurchaseRequisition } from '@/src/serv
 import ZSBService from '../templates/purchase-request/ZSBService'
 import ZSBAsset from '../templates/purchase-request/ZSBAsset'
 
-interface Props  {
-    purchaseRequisitionTypeDropdown:purchaseRequisitionTypeDropdownType[]
+interface Props {
+    purchaseRequisitionTypeDropdown: purchaseRequisitionTypeDropdownType[]
     companyDropdown: companyDropdownBasedOnUserType[]
     prData?: purchaseRequisitionDataType
     pr_id?: string
     materialDropdown: PurchaseRequisitionMaterialDropdownType[] | []
-        plantDropdown: purchaseRequisitionPlantDropdownType[]
+    plantDropdown: purchaseRequisitionPlantDropdownType[]
 }
 
-enum PurchaseType  {
+enum PurchaseType {
     nbNormal = "NB-Normal",
     nbCapex = "NB-CAPEX",
     zsbService = "ZSB Cost Centre",
     zsbAsset = "ZSB Asset",
 }
 
-const PrRequest = (props:Props) => {
-    const [prData,setPrData] = useState<purchaseRequisitionDataType>(props?.prData as purchaseRequisitionDataType);
+const PrRequest = (props: Props) => {
+    const [prData, setPrData] = useState<purchaseRequisitionDataType>(props?.prData as purchaseRequisitionDataType);
 
-    const fetchPrData = (prId?:string)=>{
-        getPurchaseReqisitionData(prId ?? props?.pr_id as string).then((res)=>{
-            console.log(res,"fetched pr data");
+    const fetchPrData = (prId?: string) => {
+        getPurchaseReqisitionData(prId ?? props?.pr_id as string).then((res) => {
+            console.log(res, "fetched pr data");
             setPrData(res);
-        }).catch((err)=>{
+        }).catch((err) => {
             console.error("Error fetching PR data:", err);
         })
     }
 
-    const handlePurchaseRequisitionSubmit = ()=>{
+    const handlePurchaseRequisitionSubmit = () => {
         confirm("Are you sure you want to submit this purchase requisition?") &&
-        submitPurchaseRequisition(props?.pr_id as string).then((res)=>{
-            alert("Purchase Requisition submitted successfully");
-            fetchPrData();
-        }).catch((err)=>{
-            console.error("Error submitting purchase requisition:", err);
-        })
+            submitPurchaseRequisition(props?.pr_id as string).then((res) => {
+                alert(res);
+                fetchPrData();
+            }).catch((err) => {
+                console.error("Error submitting purchase requisition:", err);
+            })
     }
 
-  return (
-    <div className='py-8 px-5'>
-        <CreatePurchaseRequest purchaseRequisitionTypeDropdown={props.purchaseRequisitionTypeDropdown} companyDropdown={props?.companyDropdown} prData={prData} pr_id={props?.pr_id} fetchPrData={fetchPrData}/>
-        <div className='flex justify-end'>
+    return (
+        <div className='py-8 px-5'>
+            <CreatePurchaseRequest purchaseRequisitionTypeDropdown={props.purchaseRequisitionTypeDropdown} companyDropdown={props?.companyDropdown} prData={prData} pr_id={props?.pr_id} fetchPrData={fetchPrData} />
+            <div className='flex justify-end'>
+                {
+                    props?.pr_id && !prData?.is_submitted &&
+                    <Button className='mt-5 bg-[#5291CD] text-white rounded-lg px-6 py-2 hover:bg-[#65a4e7]' onClick={() => { handlePurchaseRequisitionSubmit() }}>
+                        Submit PR
+                    </Button>
+                }
+            </div>
+
+            {/* normal pr component */}
             {
-                props?.pr_id && !prData?.is_submitted && 
-                <Button className='mt-5 bg-[#5291CD] text-white rounded-lg px-6 py-2 hover:bg-[#65a4e7]' onClick={()=>{handlePurchaseRequisitionSubmit()}}>
-          Submit PR
-        </Button>
-        }
+                props?.prData?.pr_type === PurchaseType.nbNormal && <NormalPR prData={prData} materialDropdown={props?.materialDropdown} plantDropdown={props?.plantDropdown} />
+            }
+
+            {/* capex pr component */}
+            {
+                props?.prData?.pr_type === PurchaseType.nbCapex && <CapexPR prData={prData} materialDropdown={props?.materialDropdown} plantDropdown={props?.plantDropdown} />
+            }
+
+            {/* ZSB SERVICE */}
+
+            {
+                props?.prData?.pr_type === PurchaseType.zsbService && <ZSBService prData={prData} plantDropdown={props?.plantDropdown} />
+            }
+
+            {/* ZSB ASSET */}
+
+            {
+                props?.prData?.pr_type === PurchaseType.zsbAsset && <ZSBAsset prData={prData} />
+            }
+
         </div>
-
-        {/* normal pr component */}
-        {
-            props?.prData?.pr_type === PurchaseType.nbNormal && <NormalPR prData={prData} materialDropdown={props?.materialDropdown} plantDropdown={props?.plantDropdown} />
-        }
-
-        {/* capex pr component */}
-        {
-            props?.prData?.pr_type === PurchaseType.nbCapex && <CapexPR prData={prData} materialDropdown={props?.materialDropdown} plantDropdown={props?.plantDropdown} />
-        }
-
-        {/* ZSB SERVICE */}
-
-        {
-            props?.prData?.pr_type === PurchaseType.zsbService && <ZSBService prData={prData} plantDropdown={props?.plantDropdown} />
-        }
-
-        {/* ZSB ASSET */}
-
-        {
-            props?.prData?.pr_type === PurchaseType.zsbAsset && <ZSBAsset prData={prData} />
-        }        
-
-    </div>
-  )
+    )
 }
 
 export default PrRequest
