1e34b4b588abcb17ba989bfed7ed1710e67eb393
neeludvadia <neel.udvadia@merillife.com>
2026-03-19 16:46:27 +0530

    changed column name from modified to date i both my vendor and my approval

--- src/components/molecules/Dashboard-My-Approvals-Table.tsx
+++ src/components/molecules/Dashboard-My-Approvals-Table.tsx
@@ -171,7 +171,7 @@ const DashboardMyApprovalsTable = ({ dashboardTableData, companyDropdown }: Prop
               <TableHead className="text-center">Requester</TableHead>
               <TableHead className="text-center">Country</TableHead>
               <TableHead className="text-center">Status</TableHead>
-              <TableHead className="text-center">Modified</TableHead>
+              <TableHead className="text-center">Date</TableHead>
               <TableHead className="text-center">Action</TableHead>
             </TableRow>
           </TableHeader>
